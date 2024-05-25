3e11dd59c36ca4fad591aed7a33c5b2b5c5534a4
readesd <ragarwala031@gmail.com>
2024-05-26 01:11:10 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('userData', (data) => {
+        console.log('Received user data:', data);
+        io.emit('userData', data); // Broadcast the data to all connected clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
